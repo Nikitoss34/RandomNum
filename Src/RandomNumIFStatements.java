52e01ee9687bd3fc57b492a5771460c5dce5cd1a
Mykyta <honcharov.nikita70@gmail.com>
2023-10-15 19:29:46 +0200

    first commit

---
+++ Src/RandomNumIFStatements.java
@@ -0,0 +1,22 @@
+public class RandomNumIFStatements {
+    public static void main(String[] args) {
+       // Random random = new Random();
+        //int x = random.nextInt();
+        //System.out.println(x);
+        //Random random = new Random();
+        //boolean y = random.nextBoolean();
+        //System.out.println(y);
+        int age = 15;
+        if(age==20) {
+            System.out.println("You are a teenager!");
+        }
+            else
+                if(age<=20) {
+                    System.out.println("You are a child!");
+                }
+                    else
+                  if(age>=20){
+                      System.out.println("You are an adult!");
+        }
+    }
+}
